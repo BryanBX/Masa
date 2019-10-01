a5bc32f63f2eca91225f5fceee137c8fdea22744
BryanBX <bryangp93@gmail.com>
2019-10-01 15:49:53 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double presion, volumen, temperatura, masa;
+
+            Console.WriteLine("Digite la presion");
+            presion = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el volumen");
+            volumen = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la temperatura");
+            temperatura = double.Parse(Console.ReadLine());
+
+            masa = (presion * volumen) / (0.37 * (temperatura + 460));
+            Console.WriteLine("La masa es: {0}", masa);
+            Console.ReadKey();
+        }
+    }
+}
